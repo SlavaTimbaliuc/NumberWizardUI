0b320e3680a5b426de3ec8c0adb7abd0e992af5f
Veaceslav Timbaliuc <slava@symbilityintersect.com>
2018-03-02 15:31:44 -0500

    added basic flow of the game

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -6,9 +6,11 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log ("Level load requested: " + name);
+		Application.LoadLevel(name);
 	}
 
 	public void QuitRequest() {
 		Debug.Log ("Quit Requested");
+		Application.Quit();
 	}
 }
